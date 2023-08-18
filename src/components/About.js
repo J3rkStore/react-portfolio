2f0d37361dcbd2d04d90754cdded9bfd5fac4fcc
J3rkStore <taran.leu@outlook.com>
2023-08-17 21:23:31 -0600

    Update About.js
    updating about me

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,16 @@ function About() {
     <div>
       <h2>About</h2>
       <p>
-        Hello! I'm Taran. I made this website about me. Narcisism much? Yes!
+        Hello! I'm Taran. I made this website to showcase my web developement work.
       </p>
 
       <p>
-        I am a fullstack web developer with experience in React, MongoDB,
+        I am a fullstack web developer experienced in React, MongoDB,
         GraphQL, MongoDB, and more. I recently graduated from the University of
         Utah's fullstack coding bootcamp and focused on learning and practicing
-        my skills as much as possible
-      </p>
+        my skills as much as possible.  In the interum,, I've been spending as 
+          much personal time as I can working on projects and honing my skills.  
+          Lately I've been focused on enhancing my 5 day weather forcast app, [Weatheringo.](https://github.com/J3rkStore/toad-smoker)
       <p>
         I have over 10 years IT experience and I'm working to make web
         developement my primary full-time ocupation by building interesting
